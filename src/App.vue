4e94ff870918c5cc581baf4b47358a8c4bfb15d6
nsw2535@gmail.com <nsw2535@gmail.com>
2025-09-28 17:56:48 +0900

    vue 기본 작성법 익히기

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  
+  <h1>영화정보</h1>
+  <div v-for="(movie, i) in data" :key="i" class="item">
+    <figure>
+      <img :src="`${movie.imgUrl}`" :alt="movie.title">
+    </figure>
+    <div class="info">
+      <h3 class="bg-yellow">{{ movie.title }}</h3>
+      <p>개봉: {{ movie.year }}</p>
+      <p>장르: {{ movie.category }}</p>
+      <button @click="increseLike(i)">좋아요</button> <span>{{ movie.like }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-
+  import data from './assets/movies';
+  
+  export default {
+    name: 'App',
+    data() {
+      return {
+        data: data
+      }
+    },
+    methods: { // 함수
+      increseLike(i) {
+        this.data[i].like += 1;
+      }
+    }
+  }
 </script>
 
 <style>
-
+.bg-yellow {
+  background: yellow;
+}
 </style>
---
+++ src/assets/movies.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+    title: "노량",
+    year: 2023,
+    category: "액션",
+    like: 0,
+    imgUrl: './assets/노량.jpg'
+    },
+    {
+    title: "아쿠아맨",
+    year: 2025,
+    category: "판타지",
+    like: 0,
+    imgUrl: './assets/아쿠아맨.jpg'
+    }
+]
+
+export default data;
